9ad4ed1184f74235fe71d156ccc3b0e1a7133fc3
Valentin <valentinmusset@pm.me>
2025-11-09 16:21:44 +0100

    ✨ feat: add player character with movement controls and tilemap sprite

---
+++ scripts/player.gd
@@ -0,0 +1,17 @@
+class_name Player extends CharacterBody2D
+
+
+const SPEED = 100.0
+
+
+func _physics_process(delta: float) -> void:
+	# Get the input direction for all four directions (left, right, up, down)
+	var direction := Input.get_vector("left", "right", "up", "down")
+	# Move the player
+	if direction:
+		velocity = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
+		velocity.y = move_toward(velocity.y, 0, SPEED * delta)
+
+	move_and_slide()
